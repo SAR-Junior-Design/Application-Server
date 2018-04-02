5c24088cc4078f3b547a726b4a3dbb7c5538afa1
Bruce Crane <samcrane8@gmail.com>
2018-04-02 13:25:55 -0400

    Merge branch 'mailing-admin'
    # Conflicts:
    #	Flask/.DS_Store
    #	Flask/main.py

--- Flask/Models/Document_DBModel.py
+++ Flask/Models/Document_DBModel.py
@@ -1,21 +1,17 @@
 import json
-import uuid
 from flaskapp import db, app
 from sqlalchemy.dialects.postgresql import JSON
-from flask import request, Response, send_file, send_from_directory
-from Utility.color_print import ColorPrint
-import datetime
-from Utility.Encryptor import Encryptor
 
-class Document_DBModel(db.Model):
-	__tablename__ = 'document'
-	id = db.Column(db.Text, primary_key=True)
-	owner = db.Column(db.Text, db.ForeignKey('users.id', ondelete = 'CASCADE'))
-	location = db.Column(db.Text)
-	document_type = db.Column(db.Text)
 
-	def __init__(self, id, owner, location, document_type):
-		self.id = id
-		self.owner = owner
-		self.location = location
-		self.document_type = document_type
+class Permissions_DBModel(db.Model):
+    __tablename__ = 'documents'
+    id = db.Column(db.Integer, primarykey=True)
+    owner = db.Column(db.Text)
+    document_type = db.Column(db.Text, nullable=False)
+    location = db.Column(db.Text, nullable=False)
+
+    def __init__(self, owner, document_type, location):
+        self.owner = owner
+        self.document_type = document_type
+        self.location = location
+
