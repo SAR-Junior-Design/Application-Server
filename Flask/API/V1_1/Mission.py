2414e263506ec240187f2c3f69a8c720b74156eb
Bruce Crane <samcrane8@gmail.com>
2018-04-04 18:53:48 -0400

    get possible mission conflicts
    - returns all possible mission conflicts
    - only temporal, does not consider geospatial things (because we still
    using GeoJSON and not a dedicated location data type yet).

--- Flask/API/V1_1/Mission.py
+++ Flask/API/V1_1/Mission.py
@@ -255,9 +255,59 @@ class Mission():
             return Response(return_string, status=400, mimetype='application/json')
 
 
+    @staticmethod
+    def get_possible_mission_conflicts():
+        if 'user' in session.keys():
+            user = session['user']
+
+            parsed_json = request.get_json()
+
+            if "starts_at" not in parsed_json.keys():
+                dict_local = {'message': "No starts_at attribute."}
+                return_string = json.dumps(dict_local, sort_keys=True, indent=4, separators=(',', ': '))
+                return Response(return_string, status=400, mimetype='application/json')
+
+            if "ends_at" not in parsed_json.keys():
+                dict_local = {'message': "No ends_at attribute."}
+                return_string = json.dumps(dict_local, sort_keys=True, indent=4, separators=(',', ': '))
+                return Response(return_string, status=400, mimetype='application/json')
+
+            starts_at = parsed_json["starts_at"]
+            starts_at = datetime.datetime.strptime(starts_at, "%Y-%m-%d %H:%M:%S")
+            ends_at = parsed_json["ends_at"]
+            ends_at = datetime.datetime.strptime(ends_at, "%Y-%m-%d %H:%M:%S")
+
+            missions = Mission_DBModel.query.filter(starts_at < Mission_DBModel.starts_at, Mission_DBModel.ends_at < ends_at).all()
+
+            mission_list = []
+            for mission in missions:
+
+                drones = Drone_DBModel.query.join(Asset_DBModel).filter(Drone_DBModel.id == Asset_DBModel.drone_id).filter(Asset_DBModel.mission_id == mission.id).all()
+                
+                drone_list = []
+                for drone in drones:
+                    drone_list += [{'id': drone.id, 'description': drone.description}]
+
+                commander = User_DBModel.query.filter_by(id = mission.commander).first()
+
+                mission_list += [{'starts_at': str(mission.starts_at), 'ends_at': str(mission.ends_at),
+                'area': mission.area}]
+
+            return_string = json.dumps(mission_list, sort_keys=True, indent=4, separators=(',', ': '))
+            return return_string
+
+        else:
+            dict_local = {'message': "Auth error."}
+            return_string = json.dumps(dict_local, sort_keys=True, indent=4, separators=(',', ': '))
+            return Response(return_string, status=400, mimetype='application/json')
+
+
+
+
 app.add_url_rule('/v1_1/get_missions', '/v1_1/get_missions', Mission.get_missions, methods=['POST'])
 app.add_url_rule('/v1_1/get_active_missions', '/v1_1/get_active_missions', Mission.get_active_missions, methods=['GET'])
 app.add_url_rule('/v1_1/get_past_missions', '/v1_1/get_past_missions', Mission.get_past_missions, methods=['GET'])
+app.add_url_rule('/v1_1/get_possible_mission_conflicts', '/v1_1/get_possible_mission_conflicts', Mission.get_possible_mission_conflicts, methods=['POST'])
 
 
 
